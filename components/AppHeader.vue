9ff91f4472323fc51b0b664d6d1ebbc17e041109
gidiz <gidizavaro@gmail.com>
2026-06-11 23:35:30 +0300

    Remove treatments dropdown menu from navigation

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -12,55 +12,7 @@
       <nav class="nav-menu" :class="{ open: isOpen }">
         <NuxtLink to="/" @click="closeMenu">דף הבית</NuxtLink>
         <NuxtLink to="/about" @click="closeMenu">אודות</NuxtLink>
-
-        <div
-          class="nav-menu__group"
-          @mouseenter="openTreatmentsMenu"
-          @mouseleave="closeTreatmentsMenu"
-        >
-          <button
-            class="nav-menu__trigger"
-            type="button"
-            :aria-expanded="isTreatmentsOpen"
-            @click="toggleTreatmentsMenu"
-          >
-            טיפולים
-          </button>
-
-          <div class="nav-dropdown" :class="{ open: isTreatmentsOpen }">
-            <div class="nav-dropdown__intro">
-              <NuxtLink to="/treatments" @click="closeMenu">
-                לכל תחומי הטיפול
-              </NuxtLink>
-              <p>ניווט היררכי לקטגוריות ולדפי השירות המרכזיים באתר.</p>
-            </div>
-
-            <div class="nav-dropdown__grid">
-              <div
-                v-for="category in treatmentCategories"
-                :key="category.slug"
-                class="nav-dropdown__column"
-              >
-                <NuxtLink
-                  :to="`/${category.slug}`"
-                  class="nav-dropdown__title"
-                  @click="closeMenu"
-                >
-                  {{ category.title }}
-                </NuxtLink>
-                <NuxtLink
-                  v-for="service in category.services"
-                  :key="service.slug"
-                  :to="`/${category.slug}/${service.slug}`"
-                  class="nav-dropdown__link"
-                  @click="closeMenu"
-                >
-                  {{ service.navTitle }}
-                </NuxtLink>
-              </div>
-            </div>
-          </div>
-        </div>
+        <NuxtLink to="/treatments" @click="closeMenu">טיפולים</NuxtLink>
 
         <NuxtLink to="/faq" @click="closeMenu">שאלות נפוצות</NuxtLink>
         <NuxtLink to="/contact" @click="closeMenu">יצירת קשר</NuxtLink>
@@ -73,10 +25,8 @@
 <script setup>
 import { onBeforeUnmount, onMounted, ref } from "vue";
 
-const { treatmentCategories } = useTreatmentCatalog();
 const isOpen = ref(false);
 const isScrolled = ref(false);
-const isTreatmentsOpen = ref(false);
 const SCROLL_ENTER_THRESHOLD = 48;
 const SCROLL_EXIT_THRESHOLD = 12;
 
@@ -102,20 +52,7 @@ onBeforeUnmount(() => {
   window.removeEventListener("scroll", updateScrollState);
 });
 
-const openTreatmentsMenu = () => {
-  isTreatmentsOpen.value = true;
-};
-
-const closeTreatmentsMenu = () => {
-  isTreatmentsOpen.value = false;
-};
-
-const toggleTreatmentsMenu = () => {
-  isTreatmentsOpen.value = !isTreatmentsOpen.value;
-};
-
 const closeMenu = () => {
   isOpen.value = false;
-  isTreatmentsOpen.value = false;
 };
 </script>
